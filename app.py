1477654967b480326a48b4e29d6c0996f5d1be14
elea77 <eleacarton@hotmail.fr>
2020-05-04 15:31:54 +0200

    install requests + autre

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template
+
+
+# Instancier notre application dont le nom est __main__
+app = Flask(__name__)
+
+# API
+# r = requests.get('https://api.themoviedb.org/3/movie/550?api_key=6590c29cf14027ffe0cf70d4c826f104&append_to_response=videos,images')
+
+
+@app.route('/')
+def index():
+    titre = "Ceci est la page d'accueil du site"
+    return render_template('pages/index.html', titre=titre)
+    
+# L’application démarre à partir de cette ligne.
+if __name__=='__main__':
+
+	# Le mode debug est bien pratique pour développer, mais à ne surtout par laisser quand votre site sera disponible sur Internet.
+    app.run(debug=True)
+
+    # L'objet app est configurable. On peut par exemple lui configurer sa clé secrète (qui sera indispensable pour sécuriser les sessions des visiteurs).
+    # app.secret_key = '6590c29cf14027ffe0cf70d4c826f104'
+
+# On lance l’application à partir de la console (cmd et non un shell Python)
+# python path\to_appFlask.py
+# Un message indique qu’un serveur web écoute sur l’interface locale sur le port 5000. On peut Vérifier le contenu de la page avec un navigateur web.
+
+
+
