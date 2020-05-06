6484206aebae0bdd5761944726503864f0a9c7c6
elea77 <eleacarton@hotmail.fr>
2020-05-06 22:32:12 +0200

    creation de la bdd

--- app.py
+++ app.py
@@ -1,12 +1,47 @@
 from flask import Flask, render_template, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import func
 import requests, json
+
 
 
 # Instancier notre application dont le nom est __main__
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@localhost/bdd_orm'
 db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(50), unique=False, nullable=False)
+    avatar = db.Column(db.String(120), unique=False, nullable=False)
+    date = db.Column(db.DateTime(timezone=True), server_default=func.now())
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+
+class Review(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    review = db.Column(db.Text, unique=False, nullable=False)
+    date = db.Column(db.DateTime(timezone=True), server_default=func.now())
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user = db.relationship("User", backref=db.backref("user", uselist=False))
+
+    def __repr__(self):
+        return '<Review %r>' % self.review
+
+
+class Favorite_movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    id_movie = db.Column(db.Integer, unique=False, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user = db.relationship("User", backref=db.backref("user", uselist=False))
+
+    def __repr__(self):
+        return '<Favorite_movie %r>' % self.id_movie
 
 # API
 # r = requests.get('https://api.themoviedb.org/3/movie/550?api_key=6590c29cf14027ffe0cf70d4c826f104&append_to_response=videos,images')
@@ -42,19 +77,6 @@ def movie():
 def page_not_found(error):
     return render_template('pages/404.html')
     
-# L’application démarre à partir de cette ligne.
+
 if __name__=='__main__':
-
-	# Le mode debug est bien pratique pour développer, mais à ne surtout par laisser quand votre site sera disponible sur Internet.
     app.run(debug=True)
-
-    # L'objet app est configurable. On peut par exemple lui configurer sa clé secrète (qui sera indispensable pour sécuriser les sessions des visiteurs).
-    # app.secret_key = '6590c29cf14027ffe0cf70d4c826f104'
-
-# On lance l’application à partir de la console (cmd et non un shell Python)
-# python path\to_appFlask.py
-#py -3 app.py / flask run / python3 app.py
-# Un message indique qu’un serveur web écoute sur l’interface locale sur le port 5000. On peut Vérifier le contenu de la page avec un navigateur web.
-
-
-
